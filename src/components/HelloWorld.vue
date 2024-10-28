0d46fc82f29cdcdd4dd4e2810380617ad632f815
IvanKharkivskyi1 <ikcomua@gmail.com>
2024-10-28 14:16:24 +0200

    installed tailwind

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
+      For a guide and recipes on how to configure / customize this project, Ivan<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
@@ -28,6 +28,10 @@
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
   </div>
+    <div class="bg-blue-500 text-white text-center p-4">
+      <h1 class="text-3xl font-bold">Hello, Tailwind with Vue!</h1>
+    </div>
+  
 </template>
 
 <script>
